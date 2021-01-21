6d5df32a43bc581d54a5d78deb56b1bfdbdd8dd0
Kenneth Okereke <kenneth.okereke@wagner.edu>
2021-01-21 16:40:52 -0500

    portfolio change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 
-import './App.css';
+import React from 'react'
+import {Router} from 'react-router-dom'
+import ContentWrapper from './Component/ContentWrapper';
+import Navbar from './Component/Navbar';
+import History from './History';
+import './stylesheet/App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+     return (
+        <div id="App">
+           <Router history={History}>
+              <Navbar/>
+              <ContentWrapper/>
+           </Router>
+        </div>
+     );
+  }
 }
 
 export default App;
+
---
+++ src/Component/ContentWrapper.js
@@ -0,0 +1,65 @@
+import React from 'react'
+
+import { Switch, Route, withRouter } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
+import SimpleReactLightbox from "simple-react-lightbox";
+
+import Portfolio from './Portfolio';
+import Resume from './Resume';
+
+import Portfolio from './Portfolio';
+
+import johnsonjohnson from './johnsonjohnson';
+import lululemon from './lululemon';
+import sweeper from './sweeper';
+import aetna from './aetna';
+import Snapchat from './Snapchat';
+
+
+class ContentWrapper extends React.Component {
+   isMainPage(currentRoute) {
+      return ['/', '/contact', '/resume'].includes(this.props.location.pathname);
+   }
+
+   constructor(props) {
+        super(props)
+        this.myRef = React.createRef()
+   }
+
+   componentDidUpdate(prevProps) {
+      if (this.props.location !== prevProps.location) {
+         this.myRef.current.scrollTop = 0;
+      }
+   }
+
+   render() {
+      return (
+         <SimpleReactLightbox>      
+            <div id="wrapper" className={this.isMainPage() ? 'bigNav' : 'smallNav'} ref={this.myRef}>
+               <TransitionGroup className="transition-group">
+                  <CSSTransition
+                  key={this.props.location.key}
+                  timeout={{ enter: 600 }}
+                  classNames="fade"
+                  >
+                     <section className="route-section">
+                        <Switch location={this.props.location}>
+                           <Route exact path="/" component={Portfolio} />
+                           <Route path="/resume" component={Resume} />
+                           <Route path="/johnsonAndJohnson" component={johnsonjohnson} />
+                           <Route path="/lululemon" component={lululemon} />
+                           <Route path="/Sweeper" component={sweeper} />
+                           <Route path="/aetna" component={aetna} />
+                           <Route path="/Snapchat" component={Snapchat} />
+                           
+                        </Switch>
+                     </section>
+                  </CSSTransition>
+               </TransitionGroup>
+            </div>
+         </SimpleReactLightbox>
+      );
+   }
+}
+
+export default withRouter(ContentWrapper);
